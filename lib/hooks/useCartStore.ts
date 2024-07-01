e18d1f669bb61ae8dc5e21c95e51c0f76aa7ed23
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-01 18:54:52 +0200

    added useCartStore hook

---
+++ lib/hooks/useCartStore.ts
@@ -0,0 +1,65 @@
+import { create } from 'zustand'
+import { round2 } from '../utils'
+import { OrderItem } from '../models/OrderModel'
+
+const IVA_PERCENTAGE = 0.21
+
+type Cart = {
+  items: OrderItem[]
+  itemsPrice: number
+  taxPrice: number
+  shippingPrice: number
+  totalPrice: number
+}
+
+const initialState: Cart = {
+  items: [],
+  itemsPrice: 0,
+  taxPrice: 0,
+  shippingPrice: 0,
+  totalPrice: 0
+}
+
+export const cartStore = create<Cart>(() => initialState)
+
+const calcPrice = (items: OrderItem[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + item.price * item.qty, 0)
+    ),
+    // free shipping for price over 100€
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 100),
+    taxPrice = round2(Number(IVA_PERCENTAGE * itemsPrice)),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice)
+  return { itemsPrice, shippingPrice, taxPrice, totalPrice }
+}
+
+const useCartService = () => {
+  const { items, itemsPrice, taxPrice, shippingPrice, totalPrice } = cartStore()
+  return {
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    // function to add or increase an item in the cart
+    increase: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug)
+      const updatedCartItems = exist
+        ? items.map((x) =>
+            x.slug === item.slug ? { ...exist, qty: exist.qty + 1 } : x
+          )
+        : [...items, { ...item, qty: 1 }]
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems)
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice
+      })
+    }
+  }
+}
+
+export default useCartService
