a63a42d852c42a0ebf52cb84543acd7cb30e0ba8
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-03 18:11:18 +0200

    feat: :sparkles: Added banner of featured products

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,7 @@
 import ProductItem from '@/components/products/ProductItem'
 // import data from '@/lib/data'
 import productService from '@/lib/services/productService'
-import data from '@/lib/data'
+import Link from 'next/link'
 
 import React from 'react'
 
@@ -13,6 +13,41 @@ const Home = async () => {
 
   return (
     <>
+      <div className="w-full carousel rounded-box mt-4">
+        <h2 className="text-2xl py-2">Productos destacados</h2>
+        {featuredProducts.map((product, index) => (
+          <div
+            key={product._id}
+            id={`slide-${index}`}
+            className="carousel-item relative w-full"
+          >
+            <Link href={`/products/${product.slug}`}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={product.banner} alt={product.name} className="w-full" />
+            </Link>
+
+            <div className="absolute flex justify-between transform-translate-y-1/2 left-5 right-5 top-1/2">
+              <a
+                href={`#slide-${
+                  index === 0 ? featuredProducts.length - 1 : index - 1
+                }`}
+                className="btn btn-circle"
+              >
+                ❮
+              </a>
+              <a
+                href={`#slide-${
+                  index === featuredProducts.length - 1 ? 0 : index + 1
+                }`}
+                className="btn btn-circle"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+
       <h2 className="text-2xl py-2">Últimos productos</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {/* {data.products.map((product) => ( */}
