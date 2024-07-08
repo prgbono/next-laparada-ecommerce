f4277e7f67205b4e3b9b96ea376ea71b84e5bb55
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-08 18:20:38 +0200

    feat: :children_crossing: Sign in form

---
+++ app/(front)/signin/Form.tsx
@@ -0,0 +1,108 @@
+'use client'
+import { signIn, useSession } from 'next-auth/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+const Form = () => {
+  const { data: session } = useSession()
+
+  const params = useSearchParams()
+  let callbackUrl = params.get('callbackUrl') || '/'
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<Inputs>({
+    defaultValues: { email: '', password: '' }
+  })
+
+  useEffect(() => {
+    if (session && session.user) {
+      console.log('User already logged in:', session.user)
+      router.push(callbackUrl)
+    }
+  }, [callbackUrl, params, router, session])
+
+  const formSubmit: SubmitHandler<Inputs> = async (form) => {
+    const { email, password } = form
+    signIn('credentials', { email, password })
+  }
+
+  return (
+    <div className="max-w-sm mx-auto card bg-base-300 my-4">
+      <div className="card-body">
+        <h1 className="card-title">Sign In</h1>
+        {params.get('error') && (
+          <div className="alert text-error">
+            {params.get('error') === 'CredentialsSignin'
+              ? 'Correo o contraseña incorrectos'
+              : params.get('error')}
+          </div>
+        )}
+        {params.get('sucess') && (
+          <div className="alert text-success">{params.get('success')}</div>
+        )}
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="my-2">
+            <label className="label" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              {...register('email', {
+                required: 'Por favor, introduce un email',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Por favor, introduce un email válido'
+                }
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.email?.message && (
+              <div className="text-error">{errors.email.message}</div>
+            )}
+          </div>
+          <div className="my-2">
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register('password', {
+                required: 'Por favor, introduce una contraseña'
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.password?.message && (
+              <div className="text-error">{errors.password.message}</div>
+            )}
+          </div>
+          <div className="my-4">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="btn btn-primary w-full"
+            >
+              {isSubmitting && (
+                <span className="loading loading-spinner"></span>
+              )}
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Form
